0a9cf7b88fb7446fccfc8a114e337e1e7c6dec46
Paul Deng <paulliwali@hotmail.com>
2015-03-16 20:58:32 -0400

    completed problem 2

---
+++ problem-2.py
@@ -0,0 +1,18 @@
+def fib(n):
+    initial_num = 1
+    i = 2
+    fib = []
+    fib_even_sum = 0
+    fib.append(initial_num)
+    while i < n:
+        fib.append(i)
+        intermediate_num = i
+        i = i + initial_num
+        initial_num = intermediate_num
+
+    for i in fib:
+        if i % 2 == 0:
+            fib_even_sum += i
+        print fib_even_sum
+
+fib(4000001)

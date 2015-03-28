4eb5d42cb75b33767001d07483860059dc7d88a2
Paul Deng <paulliwali@hotmail.com>
2015-03-28 12:36:22 -0400

    finished problem 3 but runtime is too long

--- problem-3.py
+++ problem-3.py
@@ -1,14 +1,38 @@
 def primeNum(n):
     prime_num = [2]
     i = 3
+    isPrime = False
+
     while i < n:
         for num in prime_num:
             if i % num == 0:
+                isPrime = False
                 break
             else:
-                prime_num.append(i)
-                break
-        i = i + 1
-    print prime_num
+                isPrime = True
 
-primeNum(10)
+        if isPrime:
+             prime_num.append(i)
+
+        isPrime = False
+        i = i + 1
+    return prime_num
+
+def factors(num, array):
+    n = num
+    factor = []
+
+    for f in array:
+        if n % f == 0:
+            factor.append(f)
+
+    return factor
+
+
+def largestPrimeFactor(n):
+    prime_num = primeNum(n)
+    prime_factors = factors(n, prime_num)
+
+    return max(prime_factors)
+
+print largestPrimeFactor(600851475143)
